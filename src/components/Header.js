5a32c7cedeee0807e2934ead4c0d82ac326d878c
Sadera Nye <saderanye1@gmail.com>
2025-05-29 13:21:28 -0700

    added pages

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Midnight Pages</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/TaskList.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function TaskList() {
+  const [tasks, setTasks] = useState([
+    { id: 1, name: 'Brush Teeth', completed: false },
+    { id: 2, name: 'Floss', completed: false },
+    { id: 3, name: 'Wear Retainer', completed: false },
+  ]);
+
+  return (
+    <div>
+      <h2>Today’s Tasks</h2>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input type="checkbox" checked={task.completed} />
+            {task.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TaskList;
